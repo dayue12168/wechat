4eeaacbcb3c6237a09fad86fcc2ebbb1e4b54856
hypnot <dayue12168@163.com>
2018-07-06 15:57:47 +0800

    豆瓣图书

---
+++ DouBanBookApp/requests/request.js
@@ -0,0 +1,44 @@
+var api = require('./api.js');
+var utils = require('../utils/util.js');
+
+/**
+ * 网路请求
+ */
+function request(url, data, successCb, errorCb, completeCb) {
+  wx.request({
+    url: url,
+    method: 'GET',
+    header: {
+      "content-type": "json"
+    },
+    data: data,
+    success: function (res) {
+      utils.isFunction(successCb) && successCb(res.data);
+    },
+    error: function () {
+      utils.isFunction(errorCb) && errorCb();
+    },
+    complete: function () {
+      utils.isFunction(completeCb) && completeCb();
+    }
+  });
+}
+
+/**
+ * 搜索图书
+ */
+function requestSearchBook(data, successCb, errorCb, completeCb) {
+  request(api.API_BOOK_SEARCH, data, successCb, errorCb, completeCb);
+}
+
+/**
+ * 获取图书详细信息
+ */
+function requestBookDokDetail(id, data, successCb, errorCb, completeCb) {
+  request(api.API_BOOK_DETAIL.replace(':id', id), data, successCb, errorCb, completeCb);
+}
+
+module.exports = {
+  requestSearchBook: requestSearchBook,
+  requestBookDokDetail: requestBookDokDetail
+}
